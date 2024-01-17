6b4cb66ffa10224f09ea7572adc3fd695b48ff7d
nienlinh <nlhsueh@mail.fcu.edu.tw>
2024-01-17 22:55:21 +0800

    simply code

--- healthUtil.py
+++ healthUtil.py
@@ -11,18 +11,3 @@ class Inbody(Enum):
     @staticmethod
     def bmi(height, weight):
         return round(weight/(height*height), 2)
-
-    @staticmethod
-    def estimatedBMR(weight, bodyFat):
-        ''' 
-        @param weight: 體重 單位公斤
-        @param bodyFat: 體脂肪率，例如 0.2
-        @rtype float: 預估的 BMR 基礎代謝
-        Katch-McArdle 依據體重和體脂肪率算出 BMR 基礎代謝 basal metabolic rate
-        BMR就是身體需要的最低熱量
-        BMR = 370 + (21.6 * Lean Body Mass [kg])
-        LBM = (Weight [kg] * (100 - Body Fat %) / 100
-        '''
-        LBM = weight * (1-bodyFat)
-        BMR = round(370 + (21.6 * LBM), 1)
-        return BMR
